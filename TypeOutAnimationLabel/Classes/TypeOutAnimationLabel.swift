5910c5332ba2d5ec4483e522a441034c8a551a78
Tejas Ardeshna <tejasardeshna@gmail.com>
2018-01-02 11:25:32 +0530

    updated for swift 3.2

--- TypeOutAnimationLabel/Classes/TypeOutAnimationLabel.swift
+++ TypeOutAnimationLabel/Classes/TypeOutAnimationLabel.swift
@@ -9,114 +9,115 @@
 import UIKit
 
 public class TypeOutAnimationLabel: UILabel {
-  enum Status: Int {
-    case Forward, Backward
-  }
-  var canBackward: Bool {
-    return currentReplacableIndex < replacableStrings.count - 1
-  }
-
-  private var fixedString = NSAttributedString()
-  private var replacableStrings = [NSAttributedString]()
-  private var currentReplacableIndex = 0
-
-  private var fixedChars = [String]()
-  private var currentReplacableChars = [String]()
-  private var status: Status = .Forward
-  private var currentIndex = 0
-  private var currentLength = 0
-
-  private func reset() {
-    fixedChars = fixedString.string.characters.map {String($0)}
-    currentReplacableIndex = 0
-    currentIndex = 0
-    initReplacableStringAtIndex(index: currentReplacableIndex)
-  }
-
-  private func initReplacableStringAtIndex(index: Int) {
-    currentReplacableChars = replacableStrings[index].string.characters.map {String($0)}
-    status = .Forward
-    currentLength = fixedChars.count + currentReplacableChars.count
-  }
-
-  func next() {
-    switch status {
-    case .Forward:
-      currentIndex += 1
-      if canBackward && currentIndex > currentLength {
-        currentIndex -= 2
-        status = .Backward
-      }
-    case .Backward:
-      if currentIndex >= fixedChars.count {
-        currentIndex -= 1
-      } else {
-        currentReplacableIndex += 1
+    enum Status: Int {
+        case Forward, Backward
+    }
+    var canBackward: Bool {
+        return currentReplacableIndex < replaceableStrings.count - 1
+    }
+    
+    private var fixedString = NSAttributedString()
+    private var replaceableStrings = [NSAttributedString]()
+    private var currentReplacableIndex = 0
+    
+    private var fixedChars = [String]()
+    private var currentReplacableChars = [String]()
+    private var status: Status = .Forward
+    private var currentIndex = 0
+    private var currentLength = 0
+    
+    private func reset() {
+        fixedChars = fixedString.string.characters.map {String($0)}
+        currentReplacableIndex = 0
+        currentIndex = 0
         initReplacableStringAtIndex(index: currentReplacableIndex)
-      }
     }
-  }
-
-  func canAnimateNext() -> Bool {
-    switch status {
-    case .Backward:
-      if currentReplacableIndex == replacableStrings.count - 1 {
-        return currentIndex <= fixedChars.count
-      }
-      return true
-    default:
-      return canBackward ? true : currentIndex <= currentLength
+    
+    private func initReplacableStringAtIndex(index: Int) {
+        currentReplacableChars = replaceableStrings[index].string.characters.map {String($0)}
+        status = .Forward
+        currentLength = fixedChars.count + currentReplacableChars.count
     }
-  }
-
-  var currentFixedString: NSAttributedString {
-    let index = [currentIndex, fixedChars.count].min()!
-    return fixedString.attributedSubstring(from: NSRange(location: 0, length: index))
-  }
-
-  var currentReplacableString: NSAttributedString? {
-    let index = [currentIndex + 1 - fixedChars.count, currentReplacableChars.count].min()!
-    if index <= 0 {
-      return nil
+    
+    func next() {
+        switch status {
+        case .Forward:
+            currentIndex += 1
+            if canBackward && currentIndex > currentLength {
+                currentIndex -= 2
+                status = .Backward
+            }
+        case .Backward:
+            if currentIndex >= fixedChars.count {
+                currentIndex -= 1
+            } else {
+                currentReplacableIndex += 1
+                initReplacableStringAtIndex(index: currentReplacableIndex)
+            }
+        }
     }
-
-    return replacableStrings[currentReplacableIndex].attributedSubstring(from: NSRange(location: 0, length: index))
-  }
-
-  func computeText() -> NSAttributedString {
-    let text = NSMutableAttributedString(attributedString: currentFixedString)
-    if let replacableString = currentReplacableString {
-      text.append(replacableString)
+    
+    func canAnimateNext() -> Bool {
+        switch status {
+        case .Backward:
+            if currentReplacableIndex == replaceableStrings.count - 1 {
+                return currentIndex <= fixedChars.count
+            }
+            return true
+        default:
+            return canBackward ? true : currentIndex <= currentLength
+        }
     }
-    return text
-  }
-
-  override public func draw(_ rect: CGRect) {
-    attributedText = computeText()
-    super.draw(rect)
-  }
-
-  public func animationWithFixedString(fixedString: NSAttributedString, replacableStrings: [NSAttributedString], typeSpeed: TimeInterval, delay: TimeInterval, completion: (() -> Void)?) {
-    self.fixedString = fixedString
-    self.replacableStrings = replacableStrings
-    reset()
-    animation(typeSpeed: typeSpeed, delay: delay, completion: completion)
-  }
-
-  func animation(typeSpeed: TimeInterval, delay: TimeInterval, completion: (() -> Void)?) {
-    if currentIndex == fixedChars.count && status == .Forward {
-      Thread.sleep(forTimeInterval: delay)
+    
+    var currentFixedString: NSAttributedString {
+        let index = [currentIndex, fixedChars.count].min()!
+        return fixedString.attributedSubstring(from: NSRange(location: 0, length: index))
     }
-    UIView.animate(withDuration: typeSpeed, delay: delay, options: .curveEaseInOut, animations: {
-      self.setNeedsDisplay()
-    }) { _ in
-      Thread.sleep(forTimeInterval: typeSpeed)
-      self.next()
-      if self.canAnimateNext() {
-        self.animation(typeSpeed: typeSpeed, delay: delay, completion: completion)
-      } else {
-        completion?()
-      }
+    
+    var currentReplacableString: NSAttributedString? {
+        let index = [currentIndex + 1 - fixedChars.count, currentReplacableChars.count].min()!
+        if index <= 0 {
+            return nil
+        }
+        
+        return replaceableStrings[currentReplacableIndex].attributedSubstring(from: NSRange(location: 0, length: index))
     }
-  }
+    
+    func computeText() -> NSAttributedString {
+        let text = NSMutableAttributedString(attributedString: currentFixedString)
+        if let replacableString = currentReplacableString {
+            text.append(replacableString)
+        }
+        return text
+    }
+    
+    override public func draw(_ rect: CGRect) {
+        attributedText = computeText()
+        super.draw(rect)
+    }
+    
+    public func animationWithFixedString(fixedString: NSAttributedString, replaceableStrings: [NSAttributedString], typeSpeed: TimeInterval, delay: TimeInterval, completion: (() -> Void)?) {
+        self.fixedString = fixedString
+        self.replaceableStrings = replaceableStrings
+        reset()
+        animation(typeSpeed: typeSpeed, delay: delay, completion: completion)
+    }
+    
+    func animation(typeSpeed: TimeInterval, delay: TimeInterval, completion: (() -> Void)?) {
+        if currentIndex == fixedChars.count && status == .Forward {
+            Thread.sleep(forTimeInterval: delay)
+        }
+        UIView.animate(withDuration: typeSpeed, delay: delay, options: .curveEaseInOut, animations: {
+            self.setNeedsDisplay()
+        }) { _ in
+            Thread.sleep(forTimeInterval: typeSpeed)
+            self.next()
+            if self.canAnimateNext() {
+                self.animation(typeSpeed: typeSpeed, delay: delay, completion: completion)
+            } else {
+                completion?()
+            }
+        }
+    }
 }
+
